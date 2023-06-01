c8a596a3181795c369008d2df39c1191d0bd6feb
anilac13 <anil4u2496@gmail.com>
2023-06-01 16:52:45 +0530

    [Anil] Add:Check given first name is valid or not

--- UserRegistrationWithLambda/Program.cs
+++ UserRegistrationWithLambda/Program.cs
@@ -5,7 +5,27 @@ namespace UserRegistrationWithLambda
     {
         static void Main(string[] args)
         {
-
+            bool flag = true;
+            Validation reg = new Validation();
+            while (flag)
+            {
+                Console.WriteLine("\nChoose option to perform \n1.Validate first name \n2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter first name");
+                        bool result = reg.ValidateFirstNameByLambda();
+                        if (result)
+                            Console.WriteLine("Valid");
+                        else
+                            Console.WriteLine("Invalid");
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
---
+++ UserRegistrationWithLambda/Validation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationWithLambda
+{
+    public class Validation
+    {
+        public const string NAME = @"^[A-Z][a-z]{2,}$";
+        public bool ValidateFirstNameByLambda() => Regex.IsMatch(Console.ReadLine(), NAME) ? true : false;
+    }
+}
